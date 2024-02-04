b0b3b031c1d3dc59d97f9322985d25de9a314d1e
pedroherreraj <pedro.herrera@uc.cl>
2024-02-04 12:00:54 -0300

    change supplier product model to supplier list product

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -2,7 +2,7 @@ module Api
   module V1
     class ProductsController < ApiController
       def index
-        json_res(Product.all, serializer: ProductBlueprint, view: :full_info)
+        json_res(Product.convenient_product, serializer: ProductBlueprint, view: :full_info)
       end
     end
   end
--- app/serializers/product_blueprint.rb
+++ app/serializers/product_blueprint.rb
@@ -2,5 +2,8 @@ class ProductBlueprint < Blueprinter::Base
   identifier :id
   view :full_info do
     fields :name
+    field :net_price do |object, _|
+      object.net_price
+    end
   end
 end
